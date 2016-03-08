840b232f7e0dde744bb30928c27298c0df253e89
s-bentley <sarah.n.bentley@hotmail.co.uk>
2016-03-08 11:39:53 +0000

    Minor fixes, prepped for other stations and tracing added.

--- read_in_CANOPUS.py
+++ read_in_CANOPUS.py
@@ -5,6 +5,8 @@ Created on Mon Sep  7 14:00:15 2015
 Read in the CANOPUS data for the GILL station (I need to expand to other stations) and save the good data in MATLAB format by month.
 I think py3???
 
+Run in Python3
+
 @author: Sarah
 
 History:
@@ -29,25 +31,27 @@ start_time = time.time()
 all_GILL_files = []
 station = 'GILL'
 
+print('Reading in files from '+station)
+
 for dirpath, dirnames, filenames in os.walk("Bentley"): #is there something quicker than os.walk?
-    for filename in [f for f in filenames if f.endswith("GILL.MAG")]:
+    for filename in [f for f in filenames if f.endswith(station+".MAG")]:
         all_GILL_files.append(os.path.join(dirpath,filename))
         
 print(all_GILL_files)
 findfile_time = time.time()
-print('Time to find GILL files '+str(findfile_time - start_time)+'s')
+print('Time to find files '+str(findfile_time - start_time)+'s')
         
         
 # Now read in good data from all these files, add to file
-years = np.arange( 1990, 2005 )
+years = np.arange(1990,2006) #np.arange( 1990, 2006 )
 months = np.arange(1,13)
 print(years)
 
 
 max_yr_entries = 365*24*60*60/5
-max_mth_entries = (31+1)*24*60*60/5
+max_mth_entries = (31+4)*24*60*60/5
 for year in years:
-    print('Doing GILL files for year ',year)
+    print('Doing files for year ',year)
     
     for month in months:
         month_data = np.zeros((max_mth_entries,9))
@@ -57,13 +61,16 @@ for year in years:
         fname_out = os.path.join(os.getcwd(),"data/raw/"+str(station)+'_'+str(year)+'_'+str(month)+'.mat')
         if os.path.isfile( fname_out ):
             os.remove(fname_out)
-
+        
         for GILL_data in all_GILL_files:
             end = len(GILL_data)
+  
             #print(GILL_data[end-16:end-12],GILL_data[end-12:end-10])
             if GILL_data[end-16:end-12] == str(year) and int( GILL_data[end-12:end-10] ) == month:
                 with open(GILL_data) as this_file:
-        
+                
+                    if month_data_entry > 31*24*60*60/5:
+                        print('Warning: seems to be too much data in month ',month)
                     for line in this_file:
                         if len(line) > 44:
                             if line[0] != '#' and line[45] == '.':
@@ -72,7 +79,7 @@ for year in years:
                                 line_data = [int(line[0:4]), int(line[4:6]), int(line[6:8]), float(line[8:10]), int(line[10:12]), int(line[12:14]), float(line[14:24]), float(line[24:34]),float(line[34:44])]
                                 month_data[month_data_entry,:] = line_data
                                 month_data_entry += 1
-                                
+                              
              
         #print('Saved file ',fname_out)
         scipy.io.savemat(fname_out,mdict={'data':month_data})
