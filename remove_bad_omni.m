9d822b7f9add81d3285242af33de22f41bbd7eda
s-bentley <sarah.n.bentley@hotmail.co.uk>
2016-03-08 11:41:50 +0000

    Bug fix: Don't break if a file doesn't exist. Just got to next.

--- remove_bad_omni.m
+++ remove_bad_omni.m
@@ -11,7 +11,7 @@ function [] = remove_bad_omni( data_dir,station,years,months)
 
     
     load(strcat(data_dir,sprintf('%s_omni',station)));
-    omni_all = omni_data; %manyx4: DATENUM      SW SPEED        Kp VALUE        E-AMPLITUDE Note the datenum should be for the very beginning of that hour.
+    omni_all = omni_data; %manyx4: DATENUM      SW SPEED    Flow pressure    proton density    Note the datenum should be for the very beginning of that hour.
     omni_size = size(omni_data);
 
 
@@ -19,37 +19,41 @@ function [] = remove_bad_omni( data_dir,station,years,months)
     for year = years
         for month = months
             mini_omni = [];
-            word_temp = sprintf('remove_bad_omni: Doing year %d, month %d',year, month);
-            disp(word_temp);
             f_to_open = strcat(data_dir,sprintf('/sorted2/%s_%d_%d',station,year,month));
-            f_to_save = strcat(data_dir,sprintf('/ready/%s_%d_%d',station,year,month));
-            load(f_to_open);
-            
-            [y m d h] = datevec( data(1,1,:) );
-            hour_dates = datenum( [y' m' d' h' zeros(size(y))' zeros(size(y))']  );
-            
-            [y m d h ] = datevec( omni_data(:,1) );
-            omni_dates = datenum( [y m d h zeros(size(y)) zeros(size(y))]  );
-            
-            mini_omni = zeros( length(hour_dates), 4);
-            
-            for i = [1:length(hour_dates)]
-                this_hour = hour_dates(i);
-                matching = omni_dates == this_hour;
-                if sum( matching ) == 1
-                    mini_omni( i,1:4 ) = omni_data( matching,1:4);
-                end
-            end
-        
-            % now find whether any rows are zero in mini_omni
-            zeroes = sum(mini_omni,2) == 0;
-            
-            % remove these
-            mini_omni( zeroes,: ) = [];
-            data(:,:,zeroes) = [];
-            
-           % save the data
-           save(f_to_save,'data','mini_omni');
+			if exist(strcat(f_to_open,'.mat')) ~= 2
+				warning(sprintf('>>> Could not load file <<< %s',f_to_open));
+			else
+				f_to_save = strcat(data_dir,sprintf('/ready/%s_%d_%d',station,year,month));
+				word_temp = sprintf('remove_bad_omni: Doing year %d, month %d',year, month);
+				disp(word_temp);
+				load(f_to_open);
+				
+				[y m d h] = datevec( data(1,1,:) );
+				hour_dates = datenum( [y' m' d' h' zeros(size(y))' zeros(size(y))']  );
+				
+				[y m d h ] = datevec( omni_data(:,1) );
+				omni_dates = datenum( [y m d h zeros(size(y)) zeros(size(y))]  );
+				
+				mini_omni = zeros( length(hour_dates), 4);
+				
+				for i = [1:length(hour_dates)]
+					this_hour = hour_dates(i);
+					matching = omni_dates == this_hour;
+					if sum( matching ) == 1
+						mini_omni( i,1:4 ) = omni_data( matching,1:4);
+					end
+				end
+			
+				% now find whether any rows are zero in mini_omni
+				zeroes = sum(mini_omni,2) == 0;
+				
+				% remove these
+				mini_omni( zeroes,: ) = [];
+				data(:,:,zeroes) = [];
+				
+			   % save the data
+			   save(f_to_save,'data','mini_omni');
+		    end
             
             
         end
