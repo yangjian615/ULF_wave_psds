cce24523a8d3756bce72f2275b1598a716c7ceca
s-bentley <sarah.n.bentley@hotmail.co.uk>
2016-05-17 14:23:38 +0100

    getstuffscatter now gets the means and medians of requested field rther than just speed.

--- getstuffscatter.m
+++ getstuffscatter.m
@@ -1,22 +1,17 @@
 % gets the medians, means necessary for Clare's scatterplot. Warning: done quickly only!
 
-function [meds,means] = getstuffscatter(all_data)
-	warning('>>> Only quick writing here, shoddy for later!<<<');
-	data_dir = strcat(pwd,'/data/');
-	station = 'GILL';
-	years = [1990:2004];
-	months = [1:12];
-
+function [meds,means] = getstuffscatter(all_data,coord,ps_or_psd)
+	
 	%[all_data,data_bins] = get_all_psd_data(data_dir,station,years,months);
 
 	meds = [];
 	means = [];
 
-	the_xpsds = cell2mat({all_data.xpsds});
+	what_checking = sprintf('%s%s',coord,ps_or_psd);
 	the_speed_bins = cell2mat({all_data.speed_bin});
 	for speedbin = [1:6]
 		this_bin = the_speed_bins == speedbin;
-		meds(:,speedbin) = median(cell2mat({all_data(this_bin).xpsds}),2);
-		means(:,speedbin) = mean(cell2mat({all_data(this_bin).xpsds}),2);
+		meds(:,speedbin) = median(cell2mat({all_data(this_bin).(what_checking)}),2);
+		means(:,speedbin) = mean(cell2mat({all_data(this_bin).(what_checking)}),2);
 	end
 end
