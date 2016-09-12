00ae531b03ec20d5f58ce547b8d2c9fa0ee0fdde
s-bentley <sarah.n.bentley@hotmail.co.uk>
2016-09-12 15:09:33 +0100

    TEsting for sort_by_speed_sectors

--- sort_by_speed_sectors.m
+++ sort_by_speed_sectors.m
@@ -3,18 +3,26 @@
 
 function [speed_quants,which_quant] = sort_by_speed_sectors(data,num_quants)
 
+	ptag = get_ptag();
+	do_print(ptag,2,'sort_by_speed_sectors: entering function\n');
+
 	if length(size(data)) ~=2
 		error('sort_by_speed_sectors:BadInputSize');
 	end
 
-
+	% find quantiles
 	speed_quants = quantile(data,num_quants);
-	all_speed_quants = [min(data), speed_quants, max(data)];
 	
+	% edges of "quantile bins"
+	all_speed_quants = [min(data), speed_quants, max(data)];
+	do_print(ptag,4,sprintf('sort_by_speed_sectors: have %d edges of quantile bins, hence %d bins \n',length(all_speed_quants),length(all_speed_quants)+1));
+	
+	% what to work out which is in each quantile bin
 	which_quant = nan(size(data));
 	in_this = false(size(data));
 
 	for sector = [1:num_quants+1] %you get one more sector than number fo quantiles
+		do_print(ptag,4,sprintf('sort_by_speed_sectors: doing for quantile sector %d \n',sector));
 		in_this = false(size(data));
 	
 		min_sector = all_speed_quants(sector);
@@ -32,6 +40,13 @@ function [speed_quants,which_quant] = sort_by_speed_sectors(data,num_quants)
 	% and check we got them all 
 	if sum(isnan(which_quant)) > 0
 		warning('sort_by_speed_sectors:BadSorting','some data not labelled as a sector');
+	end
+	
+	% check we got right amount within each 
+	for c_count = 1:num_quants+1
+		if abs( sum(which_quant==c_count) - ceil(length(data)/(num_quants+1) ) ) > 1
+			warning('sort_by_speed_sectors:PoorQuantileSorting','not the same amount in each quantile!');
+		end
 	end
 	
 end
---
+++ testing/test_sort_by_speed_sectors.m
@@ -0,0 +1,69 @@
+ % Usually "template" is the name of the file you're testing
+ % You must hand in testCase object to each
+ % Test results using verify (test fail or pass),assert or error (test pass or incomplete: won't run rest of script), assume (??) or fatal (kill test suite!)
+ % setup and teardown functions will run before and after all the test functions
+ % try to keep everything mathcing - names and structure!
+ 
+ % You MUST add this filename "template" to do_testing file to run in the suite.
+ 
+ 
+function tests = test_sort_by_speed_sectors %main fn so name should match filename
+	tests = functiontests(localfunctions);
+end
+ 
+%% Test functions
+% 1 UNlucky data means poor sorting
+% 2 Quantile output
+% 3 Sorted quantile bin output
+
+
+function test_sort_by_speed_sectors_1(testCase)
+% Check for warning when input results in poor quantile sorting
+
+	to_sort = [3 3 3 4 4 4 5 5 5];
+	num_quants = 3;
+
+	verifyWarning(testCase,@()sort_by_speed_sectors(to_sort,num_quants),'sort_by_speed_sectors:PoorQuantileSorting');
+
+end
+
+
+function test_sort_by_speed_sectors_2(testCase)
+%Check works for larger amoutns of data with right output of quantiles
+
+	rng('default');
+	to_sort = rand(100,1)*5;
+	num_quants= 3;
+
+	[quants,which_quants]= sort_by_speed_sectors(to_sort,num_quants);
+
+	verifyEqual(testCase,length(quants),num_quants);
+
+end
+
+
+function test_sort_by_speed_sectors_3(testCase)
+%Check works for larger amoutns of data with right number of sorted quantiles
+
+
+	to_sort = rand(100,1)*5;
+	num_quants= 3;
+
+	[quants,which_quants]= sort_by_speed_sectors(to_sort,num_quants);
+
+	verifyEqual(testCase,length(which_quants),length(to_sort));
+	verifyEqual(testCase,length(unique(which_quants)),num_quants+1);
+
+end
+
+
+
+
+% %% Optional file fixtures  
+% function setupOnce(testCase)  % do not change function name
+% % set a new path, for example
+% end
+
+% function teardownOnce(testCase)  % do not change function name
+% % change back to original path, for example
+% end
