ee5372df15c89b0ba1ac6831d42a3dbedc8ce8fd
s-bentley <sarah.n.bentley@hotmail.co.uk>
2016-09-12 15:04:47 +0100

    First functions using new quanitile_selection struct mehtod, including wrapper.

---
+++ guts_plot_median_and_quartiles.m
@@ -0,0 +1,58 @@
+% Takes in a data and plots the median and upper/ lower quartiles for th coordinate of psd data
+
+function [output] = guts_plot_median_and_quartiles( fn_st )
+	output = [];
+
+	
+	ptag = get_ptag();
+	do_print(ptag,2,'guts_plot_median_and_quartiles: entering function\n');
+	
+	
+	data = fn_st.data;
+	%fig_num = fn_st.hf;
+	selection_data = fn_st.selections;
+	coord = fn_st.extras.coord;
+	sscount = fn_st.scount;
+
+	ps = cell2mat({data.(sprintf('%sps',coord'))});
+	freqs = cell2mat({data(1).freqs})*1e3;
+	
+	quants = quantile(ps',3);
+	
+	% check size is as expected
+	if size(quants,1) ~=  3 | size(quants,2) ~= length(data(1).xps)
+		error('plot_median_and_quantiles:BadSize','unexpected matrix size for quantile output');
+	end
+	
+	low_q = quants(1,:);
+	med_q = quants(2,:);
+	upp_q = quants(3,:);
+	
+	%disp(fig_num);
+	%figure(fig_num);
+	%figure();
+	%subplot(7,1,sscount);
+	plot(freqs,med_q); hold on;
+	%plot(freqs,low_q,'--r');
+	%plot(freqs,upp_q,'--r');
+
+	% plot them all to show 
+	%for temp_count = 1:length(data)
+	%	plot(freqs,ps(:,temp_count));
+	%end
+
+
+	xlabel('Freq, mHz');
+	ylabel('PSD, (nT)^2 / mHz');
+	set(gca,'yscale','log');
+	axis([1,15,0,10e5]);
+	%title(sprintf('options set %d',fn_st.scount)); %really I want to display the selection data
+	
+end
+
+% Actually what I think I want to do is the normalised power intesnity slice plotts with x axis freq and y aixs psd for the given data.
+
+% THIS ISNT WORKING. EVERYTHING COMES OUT HTE SAME AND TITLE IS "3"
+
+	
+	
---
+++ select_quantiles.m
@@ -0,0 +1,49 @@
+% "selections" should be a cell, where each entry is a set {'ofield',num_quants,[which quants]} eg {'Np',6,[1]} would return the first sextile of Np data.
+% note that we don't save the sorted sectors which may save much time later on.
+
+
+function [output] = select_quantiles( data, selections )
+	
+	
+	ptag = get_ptag();
+	do_print(ptag,2,'select_quantiles: entering function\n');
+	%do_print(ptag,3,char(selections{:}));
+	%selections{:}
+
+	
+	sorting = data;
+	
+	% check input is as expected
+	if ~isstruct(data)
+		error('select_quantiles:BadInputType','data not a struct');
+	end
+
+	check_basic_struct(selections,'quantile_selections');
+	
+	do_print(ptag,3,sprintf('select_quantiles: %d conditions on this data \n',length(selections)));
+	
+	for s_count = 1:length(selections)
+	
+		o_f = selections(s_count).o_f;
+		num_quants = selections(s_count).num_quants;
+		which_quants = selections(s_count).which_quants;
+
+		do_print(ptag,4,sprintf('select_quantiles: field %s, %d quantiles \n',o_f,num_quants));
+		
+		[quant_vals,quants_sectors] = sort_by_speed_sectors(cell2mat({sorting.(o_f)}),num_quants);
+		
+		keep_vals = false(size(sorting));
+		
+		for keep_count = 1:length(which_quants)
+			keep_vals = keep_vals | (quants_sectors == which_quants(keep_count));
+		end
+		
+		sorting = sorting(keep_vals);
+	end
+	
+	output = sorting;
+	
+end
+		
+		
+		
